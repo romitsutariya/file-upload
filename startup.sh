02d802f2dfab0aba1e712289fc85a3e7427a6b40
Romit Sutariya <romitsutariya@gamil.com>
2021-08-21 17:36:33 +0530

    add dcoker  creatio

--- startup.sh
+++ startup.sh
@@ -3,4 +3,9 @@ apt update
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-docker buikd -t file_upload:01 .
+#this is the run docker without sudo command
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker 
+docker build -t romitsutariya/file-upload:1.0 .
+docker run -d  -p 8080:8080 --env PORT=8080 --env FILE_UPLOAD_URI=/tmp/ romitsutariya/file-upload:1.0
