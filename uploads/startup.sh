b84e0e35d402618406d58e65af1189a1f30a8bec
Romit Sutariya <romitsutariya@gamil.com>
2021-08-21 16:49:42 +0530

    remove git clone step

--- uploads/startup.sh
+++ uploads/startup.sh
@@ -3,7 +3,4 @@ apt update
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-git clone https://github.com/romitsutariya/file-upload.git
-cd file-upload
-
 docker buikd -t file_upload:01 .
